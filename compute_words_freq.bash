1b0dadabe7cde8db8d30b4d8048a2f19995d0ca9
Xiaoquan Kong <u1mail2me@gmail.com>
2018-07-12 16:10:50 +0800

    Update compute_words_freq.bash to using new spacy-dev-resource path

--- compute_words_freq.bash
+++ compute_words_freq.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../spacy-dev-resources/training/plain_word_freqs.py -n 8 token_cleaned_plain_files WORDS_FREQ.txt 
+python ./spacy-dev-resources/training/plain_word_freqs.py -n 8 token_cleaned_plain_files WORDS_FREQ.txt 
