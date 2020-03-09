0c2db174c54c9a091083b91e993e04b2c3ca84ba
Xiaoquan Kong <u1mail2me@gmail.com>
2020-03-09 14:41:46 +0800

    fix: opencc argument changed

--- convert_UD_Chinese-GSD_corpus.bash
+++ convert_UD_Chinese-GSD_corpus.bash
@@ -2,6 +2,6 @@
 
 cd corpus/UD_Chinese-GSD-master
 
-opencc -i zh_gsd-ud-train.conllu -o zh-simplified-ud-train.conllu -c zht2zhs.ini
-opencc -i zh_gsd-ud-dev.conllu -o zh-simplified-ud-dev.conllu -c zht2zhs.ini
-opencc -i zh_gsd-ud-test.conllu -o zh-simplified-ud-test.conllu -c zht2zhs.ini
+opencc -i zh_gsd-ud-train.conllu -o zh-simplified-ud-train.conllu -c t2s.json
+opencc -i zh_gsd-ud-dev.conllu -o zh-simplified-ud-dev.conllu -c t2s.json
+opencc -i zh_gsd-ud-test.conllu -o zh-simplified-ud-test.conllu -c t2s.json
